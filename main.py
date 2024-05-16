c73783c3e380380b0c188ae4f949cd5a54200fc7
infomcvalinhos <167476545+infomcvalinhos@users.noreply.github.com>
2024-05-16 18:56:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+n1 = float(input("Digite a nota 01: "))
+n2 = float(input("Digite a nota 02: "))
+n3 = float(input("Digite a nota 03: "))
+n4 = float(input("Digite a nota 04: "))
+
+media = (n1+ n2 + n3 + n4) / 4
+print(f"o valor da media e ={media}")
